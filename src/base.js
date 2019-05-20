1788742471b72fec051cd9dfd8456bfdafe3bc3f
guigui <g.desjonqueres@gmail.com>
2019-05-20 12:41:18 -0700

    Fix security issue

--- src/base.js
+++ src/base.js
@@ -3,9 +3,9 @@ import firebase from 'firebase/app'
 import 'firebase/database'
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyChNSbjbCgL098IJ36Y0u8AvEUa3KvfVEU",
-  authDomain: "chatbox-app-ff7ee.firebaseapp.com",
-  databaseURL: "https://chatbox-app-ff7ee.firebaseio.com",
+  apiKey: require('./firebase.key'),
+  authDomain: 'chatbox-app-ff7ee.firebaseapp.com',
+  databaseURL: 'https://chatbox-app-ff7ee.firebaseio.com'
 })
 
 const base = Rebase.createClass(firebase.database())
